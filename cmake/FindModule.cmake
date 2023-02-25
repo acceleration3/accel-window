98a1554ccfbf85966b7c93d3601404b8b4bad0e8
Acceleration3 <acceleration23@gmail.com>
2023-02-25 18:39:14 +0000

    Updated FindModule.cmake

--- cmake/FindModule.cmake
+++ cmake/FindModule.cmake
@@ -1,14 +1,19 @@
 include(FetchContent)
 
 function(add_accel_module name)
-    if(EXISTS ${ACCEL_MODULES_FOLDER}/${name})
-        add_subdirectory(${ACCEL_MODULES_FOLDER}/${name})
-    else()
-        FetchContent_Declare(
-            ${name}
-            GIT_REPOSITORY "https://www.github.com/ootbgames/${name}.git"
-            GIT_TAG "master"
-        )
-        FetchContent_MakeAvailable(${name})
+    message("Trying to find: ${ACCEL_MODULES_FOLDER}/${name}")
+    if(NOT TARGET ${name})
+        if(EXISTS "${ACCEL_MODULES_FOLDER}/${name}")
+            add_subdirectory("${ACCEL_MODULES_FOLDER}/${name}" )
+            message("Found")
+        else()
+            message("Not found, downloading.")
+            FetchContent_Declare(
+                ${name}
+                GIT_REPOSITORY "https://www.github.com/ootbgames/${name}.git"
+                GIT_TAG "master"
+            )
+            FetchContent_MakeAvailable(${name})
+        endif()
     endif()
 endfunction()
